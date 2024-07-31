a7eec9031c5bb6f0c252e6c5a7024d0b4ec4f2d0
Zachary Catlin <z@zc.is>
2024-07-30 20:42:52 -0400

    Added audio output
    Not that it's doing anything currently...

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_zec_demo (
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in | uio_in;  // Example
   assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_oe  = 8'b1000_0000;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
