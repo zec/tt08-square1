11f022211c2f687fc6dbea6da155e1b045231590
Zachary Catlin <z@zc.is>
2024-07-31 22:49:45 -0400

    Further work on phosphor persistence
    Also, I may have a good name for the demo.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_zec_demo (
+module tt_um_zec_square1 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -80,18 +80,52 @@ module tt_um_zec_demo (
   );
 */
 
-  // basic Munching Squares demo
+  // frame counter
   reg [8:0] frame_no;
   always @(posedge vsync) begin
     frame_no <= frame_no + 9'd1;
   end
 
-  wire [8:0] x_plot = vpos[8:0] ^ frame_no;
-  wire [8:0] x_plot2 = vpos[8:0] ^ (frame_no - 9'd5);
-  wire [8:0] x_plot3 = vpos[8:0] ^ (frame_no - 9'd10);
+  `define N_LAG 5
 
-  assign R = ((vpos < 480) & (hpos < 512)) ? (hpos[8:0] == x_plot) ? 2'b11 : (hpos[8:0] == x_plot2) ? 2'b10 : (hpos[8:0] == x_plot3) ? 2'b01 : 2'b00 : 2'b00;
-  assign G = ((vpos < 480) & (hpos < 512)) ? (hpos[8:0] == x_plot) ? 2'b11 : (hpos[8:0] == x_plot2) ? 2'b10 : (hpos[8:0] == x_plot3) ? 2'b01 : 2'b00 : 2'b00;
-  assign B = ((vpos < 480) & (hpos < 512)) ? (hpos[8:0] == x_plot) ? 2'b11 :  2'b00 : 2'b00;
+  wire [2:0] color_controls [(`N_LAG-1):0];
+  assign color_controls[0] = 3'b0_11;
+  assign color_controls[1] = 3'b1_11;
+  assign color_controls[2] = 3'b1_10;
+  assign color_controls[3] = 3'b1_01;
+  assign color_controls[4] = 3'b1_01;
+
+  wire [2:0] color_maybe [(`N_LAG-1):0];
+
+
+  genvar i;
+
+  generate
+    for (i = 0; i < `N_LAG; i = i + 1) begin
+      assign color_maybe[i] = (hpos[8:0] == (vpos[8:0] ^ (frame_no - i))) ? color_controls[i] : 3'b0_00;
+    end
+  endgenerate
+
+  wire [2:0] color;
+/*
+  genvar j;
+  generate
+    for (j = 0; j < 3; j = j + 1) begin
+      assign color[j] = |{color_maybe[(`N_LAG-1):0][j]};
+    end
+  endgenerate
+*/
+  assign color[0] = color_maybe[0][0] | color_maybe[1][0] | color_maybe[2][0] | color_maybe[3][0] | color_maybe[4][0];
+  assign color[1] = color_maybe[0][1] | color_maybe[1][1] | color_maybe[2][1] | color_maybe[3][1] | color_maybe[4][1];
+  assign color[2] = color_maybe[0][2] | color_maybe[1][2] | color_maybe[2][2] | color_maybe[3][2] | color_maybe[4][2];
+/*
+  assign color[1] = |{color_maybe[(`N_LAG-1):0][1]};
+  assign color[2] = |{color_maybe[(`N_LAG-1):0][2]};
+*/
+
+
+  assign R = ((vpos < 480) & (hpos < 512)) ? color[1:0] & {2{color[2]}} : 2'b00;
+  assign G = ((vpos < 480) & (hpos < 512)) ? color[1:0] : 2'b00;
+  assign B = ((vpos < 480) & (hpos < 512)) ? color[1:0] & {2{~color[2]}} : 2'b00;
 
 endmodule
--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  tt_um_zec_demo user_project (
+  tt_um_zec_square1 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
